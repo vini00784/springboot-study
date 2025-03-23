5a09da3097724be167dc88d3f6dce270e66bee92
Vinícius Oliveira <vo70346@gmail.com>
2025-03-23 13:13:04 -0300

    fix: move file

--- api-study/src/main/java/br/com/vini/api_study/api/controller/PlayerController.java
+++ api-study/src/main/java/br/com/vini/api_study/api/controller/PlayerController.java
@@ -1,12 +1,14 @@
-package br.com.vini.api_study.controller;
+package br.com.vini.api_study.api.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.vini.api_study.api.controller.definition.PlayerControllerDef;
+
 @RestController
 @RequestMapping("/v1/player")
-public class PlayerController {
+public class PlayerController implements PlayerControllerDef {
     
     @GetMapping("/first-message")
     public String firstMessage() {
